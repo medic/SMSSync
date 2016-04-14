a0878e53610b6a88192b69032f38b186072ee6c0
Alex Anderson <anderson@medicmobile.org>
2016-04-14 18:10:27 +0200

    Remove unused import in MainHttpClient

--- smssync/src/main/java/org/addhen/smssync/net/MainHttpClient.java
+++ smssync/src/main/java/org/addhen/smssync/net/MainHttpClient.java
@@ -30,7 +30,6 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
